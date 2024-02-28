1cc7fc76bf2e40e40cc09d9238967c101503bdb9
manveldarchinyan0 <84789287+manveldarchinyan0@users.noreply.github.com>
2024-02-28 21:36:14 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-/*
-let icon = document.querySelector("main .experience .workExperience .workExp .top .rSide .icon")
-let workExp = document.querySelector("main .experience .workExperience .workExp")
-console.log(icon.parentElement.parentElement.parentElement)
-icon.addEventListener("click",function() {
-    icon.parentElement.parentElement.parentElement.classList.toggle("open")
-})*/
-let navMenu = document.querySelector(".header .nav .navMenu")
